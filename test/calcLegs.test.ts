23df7f1633634dc9bb38ac7e2b371476e45e0f80
Philipp Niederlag <philipp.niederlag@artcom.de>
2021-12-22 16:59:29 +0100

    Extend tests

--- test/calcLegs.test.ts
+++ test/calcLegs.test.ts
@@ -7,38 +7,102 @@ import {offsets} from "../src/PoseSolver/calcLegs"
 let worldLandmarks:Results
 const PI = Math.PI
 
-describe("should resolve the correct leg rotation for", () => {
+describe("should resolve the correct upper leg rotation for", () => {
     beforeEach(() => {
         worldLandmarks = cloneDeep(poseWorldLandmarks)
       })
 
-    test("upper left leg", () => {
+
+    test("upper left leg, 90 degree forward, up", () => {
+        worldLandmarks[12] = {x:0, y: -0.4, z: 0}
+        worldLandmarks[24] = {x:0, y: 0, z: 0}
+        worldLandmarks[26] = {x:0, y: 0, z: -0.4}
+
+        const result = calcLegs(worldLandmarks)
+          
+        expect(round(result.UpperLeg.l.x)).toBe(round(PI / 2))
+        expect(round(result.Unscaled.UpperLeg.l.z)).toBe(0)
+
+        expect(round(result.UpperLeg.l.z)).toBe(round((0 - offsets.upperLeg.z)))
+        expect(round(result.Unscaled.UpperLeg.l.x)).toBe(0.5)
+    })
+
+
+    test("upper left leg, straight down", () => {
+        worldLandmarks[12] = {x:0, y: -0.4, z: 0}
+        worldLandmarks[24] = {x:0, y: 0, z: 0}
+        worldLandmarks[26] = {x:0, y: 0.4, z: 0}
+
+        const result = calcLegs(worldLandmarks)
+          
+        expect(round(result.UpperLeg.l.x)).toBe(round(0))
+        expect(round(result.Unscaled.UpperLeg.l.z)).toBe(0)
+
+        expect(round(result.UpperLeg.l.z)).toBe(round((0 - offsets.upperLeg.z)))
+        expect(round(result.Unscaled.UpperLeg.l.x)).toBe(0)
+    })
+
+    test("upper left leg, 90 degree forward, up, 45 degree left", () => {
         worldLandmarks[12] = {x:0, y: -0.4, z: 0}
         worldLandmarks[24] = {x:0, y: 0, z: 0}
         worldLandmarks[26] = {x:-0.4, y: 0, z: -0.4}
 
         const result = calcLegs(worldLandmarks)
-  
-        expect(result.UpperLeg.l.x).toBe(PI / 2)
-        expect(result.Unscaled.UpperLeg.l.x).toBe(0.5)
-  
+          
+        expect(round(result.UpperLeg.l.x)).toBe(round(PI / 2))
+        expect(round(result.Unscaled.UpperLeg.l.x)).toBe(0.5)
+
         expect(round(result.UpperLeg.l.z)).toBe(round((-PI / 4 - offsets.upperLeg.z)))
         expect(round(result.Unscaled.UpperLeg.l.z)).toBe(-0.25)
     })
 
-    test("upper right leg", () => {
+    test("upper right leg, 90 degree forward, up", () => {
         worldLandmarks[11] = {x:0, y: -0.4, z: 0}
         worldLandmarks[23] = {x:0, y: 0, z: 0}
-        worldLandmarks[25] = {x:-0.4, y: 0, z: -0.4}
+        worldLandmarks[25] = {x:0, y: 0, z: -0.4}
 
         const result = calcLegs(worldLandmarks)
   
+        expect(round(result.UpperLeg.r.x)).toBe(round(PI / 2))        
+        expect(round(result.Unscaled.UpperLeg.r.x)).toBe(0.5)
+
+        expect(round(result.UpperLeg.r.z)).toBe(round(0 + offsets.upperLeg.z))
+        expect(round(result.Unscaled.UpperLeg.r.z)).toBe(0)
+    })
+
+    test("upper right leg, straight down", () => {
+        worldLandmarks[11] = {x:0, y: -0.4, z: 0}
+        worldLandmarks[23] = {x:0, y: 0, z: 0}
+        worldLandmarks[25] = {x:0, y: 0.4, z: 0}
+
+        const result = calcLegs(worldLandmarks)
+          
+        expect(round(result.UpperLeg.r.x)).toBe(round(0))
+        expect(round(result.Unscaled.UpperLeg.r.z)).toBe(0)
+
+        expect(round(result.UpperLeg.r.z)).toBe(round((0 + offsets.upperLeg.z)))
+        expect(round(result.Unscaled.UpperLeg.r.x)).toBe(0)
+    })
+
+    test("upper right leg, 90 degree forward, up, 45 degree left", () => {
+        worldLandmarks[11] = {x:0, y: -0.4, z: 0}
+        worldLandmarks[23] = {x:0, y: 0, z: 0}
+        worldLandmarks[25] = {x:0.4, y: 0, z: -0.4}
+
+        const result = calcLegs(worldLandmarks)
+          
         expect(round(result.UpperLeg.r.x)).toBe(round(PI / 2))
         expect(round(result.Unscaled.UpperLeg.r.x)).toBe(0.5)
 
-        expect(round(result.UpperLeg.r.z)).toBe(round(-PI / 4 + offsets.upperLeg.z))
-        expect(round(result.Unscaled.UpperLeg.r.z)).toBe(-0.25)
+        expect(round(result.UpperLeg.r.z)).toBe(round((PI / 4 + offsets.upperLeg.z)))
+        expect(round(result.Unscaled.UpperLeg.r.z)).toBe(0.25)
     })
+})
+
+describe("should resolve the correct lower leg rotation for", () => {
+    beforeEach(() => {
+        worldLandmarks = cloneDeep(poseWorldLandmarks)
+      })
 
     test("lower left leg", () => {
         worldLandmarks[24] = {x:0, y: 0, z: 0}
@@ -63,7 +127,7 @@ describe("should resolve the correct leg rotation for", () => {
     })
 })
 
-function round(value:number, decimals:number = 3){
-    const factor = Math.pow(10,decimals)
+function round(value: number, decimals: number = 3){
+    const factor = Math.pow(10, decimals)
     return Math.round(value * factor) / factor
 }
