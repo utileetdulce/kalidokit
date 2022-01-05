3ec39d48153efd4364692255a3d9ae277263e7a8
Philipp Niederlag <philipp.niederlag@artcom.de>
2022-01-05 17:20:44 +0100

    Adjust offscreen detection

--- src/PoseSolver/index.ts
+++ src/PoseSolver/index.ts
@@ -56,25 +56,25 @@ export class PoseSolver {
         let Legs = enableLegs ? calcLegs(lm3d) : null;
 
         //DETECT OFFSCREEN AND RESET VALUES TO DEFAULTS
-        let rightHandOffscreen = lm3d[15].y > -0.1 || lm3d[15].visibility! < 0.23 || 0.995 < lm2d[15].y;
-        let leftHandOffscreen = lm3d[16].y > -0.1 || lm3d[16].visibility! < 0.23 || 0.995 < lm2d[16].y;
+        let rightHandOffscreen = lm3d[15].visibility! < 0.5 || 0.995 < lm2d[15].y;
+        let leftHandOffscreen = lm3d[16].visibility! < 0.5 || 0.995 < lm2d[16].y;
 
-        let leftFootOffscreen = lm3d[23]?.visibility! < 0.63 || Hips.Hips.position.z > -0.4;
-        let rightFootOffscreen = lm3d[24]?.visibility! < 0.63 || Hips.Hips.position.z > -0.4;
+        let leftFootOffscreen = lm3d[23]?.visibility! < 0.5 || Hips.Hips.position.z > -0.4;
+        let rightFootOffscreen = lm3d[24]?.visibility! < 0.5 || Hips.Hips.position.z > -0.4;
 
-        // Arms.UpperArm.l = Arms.UpperArm.l.multiply(leftHandOffscreen ? 0 : 1);
-        // Arms.UpperArm.l.z = leftHandOffscreen ? RestingDefault.Pose.LeftUpperArm.z : Arms.UpperArm.l.z; //.55 is Hands down Default position
-        // Arms.UpperArm.r = Arms.UpperArm.r.multiply(rightHandOffscreen ? 0 : 1);
-        // Arms.UpperArm.r.z = rightHandOffscreen ? RestingDefault.Pose.RightUpperArm.z : Arms.UpperArm.r.z;
+        Arms.UpperArm.l = Arms.UpperArm.l.multiply(leftHandOffscreen ? 0 : 1);
+        Arms.UpperArm.l.z = leftHandOffscreen ? RestingDefault.Pose.LeftUpperArm.z : Arms.UpperArm.l.z;
+        Arms.UpperArm.r = Arms.UpperArm.r.multiply(rightHandOffscreen ? 0 : 1);
+        Arms.UpperArm.r.z = rightHandOffscreen ? RestingDefault.Pose.RightUpperArm.z : Arms.UpperArm.r.z;
 
-        // Arms.LowerArm.l = Arms.LowerArm.l.multiply(leftHandOffscreen ? 0 : 1);
-        // Arms.LowerArm.r = Arms.LowerArm.r.multiply(rightHandOffscreen ? 0 : 1);
+        Arms.LowerArm.l = Arms.LowerArm.l.multiply(leftHandOffscreen ? 0 : 1);
+        Arms.LowerArm.r = Arms.LowerArm.r.multiply(rightHandOffscreen ? 0 : 1);
 
         Arms.Hand.l = Arms.Hand.l.multiply(leftHandOffscreen ? 0 : 1);
         Arms.Hand.r = Arms.Hand.r.multiply(rightHandOffscreen ? 0 : 1);
 
         //skip calculations if disable legs
-        if (enableLegs && Legs) {
+        if (Legs) {
             Legs.UpperLeg.l = Legs.UpperLeg.l.multiply(rightFootOffscreen ? 0 : 1);
             Legs.UpperLeg.r = Legs.UpperLeg.r.multiply(leftFootOffscreen ? 0 : 1);
             Legs.LowerLeg.l = Legs.LowerLeg.l.multiply(rightFootOffscreen ? 0 : 1);
